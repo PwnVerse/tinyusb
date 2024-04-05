43368d1d2eb7dbb0ab1fa15016df1c882190b7e3
PwnVerse <ritviktanksalkar@gmail.com>
2024-04-05 14:55:42 -0400

    Updates

--- examples/device/cdc_msc_freertos/src/main.c
+++ examples/device/cdc_msc_freertos/src/main.c
@@ -107,7 +107,7 @@ int main(void) {
 #else
   xTaskCreate(led_blinking_task, "blinky", BLINKY_STACK_SIZE, NULL, 1, NULL);
   xTaskCreate(usb_device_task, "usbd", USBD_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, NULL);
-  xTaskCreate(cdc_task, "cdc", CDC_STACK_SZIE, NULL, configMAX_PRIORITIES - 2, NULL);
+  xTaskCreate(cdc_task, "cdc", CDC_STACK_SIZE, NULL, configMAX_PRIORITIES - 2, NULL);
 #endif
 
   // skip starting scheduler (and return) for ESP32-S2 or ESP32-S3
